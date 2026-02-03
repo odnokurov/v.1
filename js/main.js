1af2ce03d0a5577c45d11bf0343c52993d9ac3af
Сергей Иванов <gr434_ivsan@lab-211-6.tomtit-tomsk.ru>
2026-02-03 11:20:20 +0700

    third

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ let app = new Vue({
         altText: "A pair of socks",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
         inStock: true,
-        inventory: 100,
+        inventory: 10,
         onSale: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
@@ -31,6 +31,9 @@ let app = new Vue({
     methods: {
         addToCart() {
             this.cart += 1
+            if (this.cart > this.inventory) {
+                this.cart = this.inventory
+            }
         },
         removeFromCart() {
             this.cart -= 1;
